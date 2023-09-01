cae212ff96d02db66f2153af0af056dfc96045f1
ChoSangYeon <chosy607@gmail.com>
2023-09-02 02:48:53 +0900

    프로필 이미지 업로드 구간 재배치

--- js_set/profile.js
+++ js_set/profile.js
@@ -1,1 +1,28 @@
-//함수구현
+// 프로필 이미지 업로드
+function setThumbnail(event) {
+  const reader = new FileReader()
+
+  reader.onload = function(event) {
+    const img = document.createElement("img");
+    img.setAttribute("src", event.target.result);
+    const imgContainer = document.querySelector("div#image_container");
+    imgContainer.innerHTML = '';
+    imgContainer.appendChild(img);
+  };
+  reader.readAsDataURL(event.target.files[0]);
+}
+
+//루틴 추천 키워드, 배열 저장
+const savedValues = [];
+
+function displayInputValue() {
+  const inputValue = document.getElementById("inputValue").value;
+  savedValues.push(inputValue);
+  updateOutputDiv();
+  document.getElementById("inputValue").value = '';
+}
+
+function updateOutputDiv() {
+  const outputDiv = document.getElementById("output-div");
+  outputDiv.innerHTML = savedValues.map(value => `<span class="value">${value}</span>`).join('');
+}
