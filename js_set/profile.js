162d014f1a4f9077dd9ff6e3d9b727f4711e0f58
ChoSangYeon <chosy607@gmail.com>
2023-09-03 03:05:25 +0900

    입력값 배열로 만들어서 출력하기

--- js_set/profile.js
+++ js_set/profile.js
@@ -5,24 +5,29 @@ function setThumbnail(event) {
   reader.onload = function(event) {
     const img = document.createElement("img");
     img.setAttribute("src", event.target.result);
-    const imgContainer = document.querySelector("div#image_container");
-    imgContainer.innerHTML = '';
-    imgContainer.appendChild(img);
+    document.querySelector("div#image_container").appendChild(img);
   };
   reader.readAsDataURL(event.target.files[0]);
 }
 
-//루틴 추천 키워드, 배열 저장
-const savedValues = [];
+// 키워드 출력
+const inputData = [];
 
-function displayInputValue() {
-  const inputValue = document.getElementById("inputValue").value;
-  savedValues.push(inputValue);
-  updateOutputDiv();
+function sendInputValue() {
+  const keyWord = document.getElementById("inputValue");
+  inputData.push(inputValue.value);
+  keyWordOutput();
   document.getElementById("inputValue").value = '';
+  console.log(inputData);
 }
 
-function updateOutputDiv() {
-  const outputDiv = document.getElementById("output-div");
-  outputDiv.innerHTML = savedValues.map(value => `<span class="value">${value}</span>`).join('');
+function keyWordOutput() {
+  const outputValue = document.getElementById("outputValue");
+  document.getElementById("outputValue").innerHTML = inputData;
+  outputValue.innerHTML = inputData.join("&nbsp&nbsp");
 }
+
+//if문을 사용한다면? if (출력값의 길이가 > .submit-box width) {줄바꿈을 해라}
+//or &nbsp로 띄우는거 말고 <br>을 사용하고, htu.html 처럼 스크롤? x -> 띄어쓰기
+// 띄어쓰기하고 적당할때 줄바꿈? if문 만이 답인가
+// outputValue의 배열을 localstorage에 저장 -> routine.html에서 꺼내오기
