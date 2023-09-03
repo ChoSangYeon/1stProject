96ae359d38866684144b5bcda63f5ef2b15ffd9f
ChoSangYeon <chosy607@gmail.com>
2023-09-03 18:32:11 +0900

    localstorage 저장

--- js_set/profile.js
+++ js_set/profile.js
@@ -1,8 +1,8 @@
 // 프로필 이미지 업로드
-function setThumbnail(event) {
+function setThumbnail() {
   const reader = new FileReader()
 
-  reader.onload = function(event) {
+  reader.onload = function() {
     const img = document.createElement("img");
     img.setAttribute("src", event.target.result);
     document.querySelector("div#image_container").appendChild(img);
@@ -25,6 +25,17 @@ function keyWordOutput() {
   const outputValue = document.getElementById("outputValue");
   document.getElementById("outputValue").innerHTML = inputData;
   outputValue.innerHTML = inputData.join("&nbsp;&nbsp;");
+}
+
+// save 버튼으로 배열을 localstorage에 저장
+function sendLocalStorage() {
+  const sendLSButton = document.querySelector('#sendLSButton');
+  sendLSButton.addEventListener('click', function() {
+    const workoutArray = inputData;
+    let makeValue = JSON.stringify(workoutArray);
+    makeValue = inputData.toString();
+    localStorage.setItem('workout', makeValue);
+  });
 }
 
 //if문을 사용한다면? if (출력값의 길이가 > .submit-box width) {줄바꿈을 해라}
