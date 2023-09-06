6b66cc27b7db3b27a42ed36d8e29e67da772f3ae
ChoSangYeon <chosy607@gmail.com>
2023-09-06 16:04:11 +0900

    완료

--- js_set/profile.js
+++ js_set/profile.js
@@ -1,15 +1,3 @@
-// 프로필 이미지 업로드
-function setThumbnail(event) {
-  const reader = new FileReader()
-
-  reader.onload = function(event) {
-    const img = document.createElement("img");
-    img.setAttribute("src", event.target.result);
-    document.querySelector("div#image_container").appendChild(img);
-  };
-  reader.readAsDataURL(event.target.files[0]);
-}
-
 // 키워드 입출력
 const inputData = [];
 
@@ -33,4 +21,11 @@ function keyWordOutput() {
 // save 버튼으로 배열을 localstorage에 저장
 function sendLocalStorage() {
   localStorage.setItem('workout', JSON.stringify(inputData));
+}
+
+// delete 버튼으로 전체 삭제
+function deleteAll() {
+  localStorage.removeItem('workout');
+  inputData.splice(0, inputData.length);
+  keyWordOutput();
 }
